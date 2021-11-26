dd25b983c498d77519e4ed60fb4d01daaeeece14
★[ᴍᴀʏᴀɴᴋ ʙʜᴀʀᴅᴡᴀᴊ]★ <80567208+Mayank50082785@users.noreply.github.com>
2021-11-26 16:31:03 +0530

    Create demo.java

---
+++ helloworld/demo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class helloworld
+{
+    public static void main( String [] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+//Add two numbers
+//AddTwoNumbers--->Pascal Naming convension
+//addTwoNumbers---->camel case naming convesion
+
