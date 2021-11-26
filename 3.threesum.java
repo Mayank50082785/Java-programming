ce81c26ab2d01daf9562f0e82a00d399faf1cc02
★[ᴍᴀʏᴀɴᴋ ʙʜᴀʀᴅᴡᴀᴊ]★ <80567208+Mayank50082785@users.noreply.github.com>
2021-11-26 22:55:37 +0530

    Update 3.threesum.java

--- 3.threesum.java
+++ 3.threesum.java
@@ -13,3 +13,20 @@ public class threenumbers
         System.out.println("Sum is " + sum);
     }
 }
+
+---------------------------------------------------------------------------------
+    
+package com.company;
+public class threenumbers
+{
+    public static void main( String [] args)
+    {
+        System.out.println("Enter three value to find the sum ");
+        int x = 183;
+        int y = 785;
+        int z = 125;
+        int sum = x + y + z;
+        System.out.println("Sum is " + sum);
+    }
+}
+
